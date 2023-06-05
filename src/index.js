5d1e3b5b07161d3e568fb4ab7517fe42b974bbfc
Chris Armbruster <chris@armbrustermail.com>
2023-06-05 18:58:22 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const { faker } = require("@faker-js/faker")
+
+const users = [...Array(50)].map((_, i) => ({
+  userId: faker.datatype.uuid(),
+  username: faker.internet.userName(),
+  email: faker.internet.email(),
+  avatar: faker.image.avatar(),
+  password: faker.internet.password(),
+  birthdate: faker.date.birthdate(),
+  registeredAt: faker.date.past(),
+}))
+
+console.log(users)
+
+module.exports = users
